dc9712a4abaefe57e06e114639a5dec0d6f9b945
1287237688 <51707476+1287237688@users.noreply.github.com>
2019-06-27 14:23:09 +0800

    update 9

--- update4.cpp
+++ update4.cpp
@@ -8,9 +8,9 @@ string number;
 string name;
 string sex;
 string major; 
-string college;
+string college; 
 double mark[7];
-double sum; 
+double sum;
 };
 
 struct Judge
@@ -21,18 +21,18 @@ struct Judge
 
 int lxg(Student &p1,Student &p2)
 {
-	if(p1.sum>p2.sum)
-	{
-		return 1 ;
-		
-	}else
-	{
-		return 0;
-	}
+    if(p1.sum>p2.sum)
+    {
+	    return 1;    
+    }
+    else
+    {
+	    return 0;
+    }	
 }
 
 int main()
-{   
+{
 	Student student[5];
 	Judge judge[7];	
 	int n=0;
@@ -41,7 +41,7 @@ int main()
     if (!lin.is_open())
     {
         cout<<"open error";
-    }
+    }	
     while(!lin.eof() && n<5)
 	{
 	lin >> student[n].number 
@@ -50,45 +50,45 @@ int main()
 		>> student[n].major
 		>> student[n].college; 
 		n++;
-	 } 
+	 } 	
 	 lin.close();
 	 n=0;
 	 ifstream gen("D:\gen.txt");
     if (!gen.is_open())
     {
         cout<<"open error";
-    }
+    }	
     while(!gen.eof() && n<7)
 	{
-		gen >>judge[n].name 
-			>>judge[n].score[0]
-			>>judge[n].score[1]
-			>>judge[n].score[2]
-			>>judge[n].score[3]
-			>>judge[n].score[4];
+		 gen >> judge[n].name
+			 >>judge[n].score[0]
+			 >>judge[n].score[1]
+			 >>judge[n].score[2]
+			 >>judge[n].score[3]
+			 >>judge[n].score[4];
 		    n++;
-	 } 
-	 
-    gen.close();
-    int i=0,j=0;
+	 } 	
+	
+ gen.close();	
+ int i=0,j=0;
  	for(i=0;i<5;i++) 
  	{
  		for(j=0;j<7;j++)
  		{
  			student[i].mark[j]=judge[j].score[i];
 		 }
-	 }
+	 }	
  	for(i=0;i<5;i++)
  	{
- 		sort(student[i].mark,student[i].mark+7);
+ 		sort( student[i].mark, student[i].mark+7);
 		for( j=1; j<6; j++ )
 		{
 			student[i].sum += student[i].mark[j];
 			}
 			student[i].sum/=5;	
 	 }
-    sort(student,student+5,lxg);
-    for(int i=0;i<5;i++)
+	sort(student,student+5,lxg);	
+	for(int i=0;i<5;i++)
 	{
 		shuchu<<student[i].college
 		      <<student[i].major
@@ -97,7 +97,7 @@ int main()
 			  <<student[i].sex
 			  <<student[i].sum
 			  <<endl;
-
+		
 	} 	
 	for(int i=0;i<5;i++)
 	{
@@ -110,5 +110,4 @@ int main()
 		<<endl;
 	} 	
 	return 0;
-	
 }
