95759d63b94ef42de526f17f6b35cbd690a0d1b7
1287237688 <51707476+1287237688@users.noreply.github.com>
2019-07-03 19:36:15 +0800

    update 12

--- update4.cpp
+++ update4.cpp
@@ -36,30 +36,30 @@ int main()
 	Student student[5];
 	Judge judge[7];	
 	int n=0;
-	ifstream lin("D:\lin.txt");
-	ofstream shuchu("D:\shuchu.xl");
-    if (!lin.is_open())
+	ifstream readin("/Users/s20181105305/Desktop/readin.txt");
+	ofstream readto("/Users/s20181105305/Desktop/readto.txt");
+    if (!readin.is_open())
     {
         cout<<"open error";
     }	
-    if(!lin.eof() && n<5)
+    if(!readin.eof() && n<5)
 	{for(n=0;n<5;n++)
-	lin >> student[n].number 
+	readin >> student[n].number 
 		 >> student[n].name
 		>> student[n].sex
 		>> student[n].major
 		>> student[n].college;
 	 } 	
-	 lin.close();
+	 readin.close();
 	 n=0;
-	 ifstream gen("D:\gen.txt");
-    if (!gen.is_open())
+	 ifstream teacher("/Users/s20181105305/Desktop/teacher.txt");
+    if (!teacher.is_open())
     {
         cout<<"open error";
     }	
-    if(!lin.eof() && n<5)
+    if(!readin.eof() && n<5)
 	{    for(n=0;n<5;n++)
-		 gen >> judge[n].name
+		 teacher >>judge[n].name
 			 >>judge[n].score[0]
 			 >>judge[n].score[1]
 			 >>judge[n].score[2]
@@ -67,7 +67,7 @@ int main()
 			 >>judge[n].score[4];
 	 } 	
 	
- gen.close();	
+ teacher.close();	
  	for(int i=0;i<5;i++) 
  	{
  		for(int j=0;j<7;j++)
@@ -82,23 +82,23 @@ int main()
 		{
 			student[i].sum += student[i].mark[j];
 			}
-			student[i].sum/=7;	
+			student[i].sum/=5;	
 	 }
 	sort(student,student+5,lxg);	
 	for(int i=0;i<5;i++)
 	{
-		shuchu<<student[i].college
-		      <<student[i].major
-			  <<student[i].number
-			  <<student[i].name
-			  <<student[i].sex
-			  <<student[i].sum
-			  <<endl;
+		readto<<<<student[i].college
+		<<" "<<student[i].major
+		<<" "<<student[i].number
+		<<" "<<student[i].name
+		<<" "<<student[i].sex
+		<<" "<<student[i].sum
+		<<endl;
 			  
 	} 	
 	for(int i=0;i<5;i++)
 	{
-		cout<<student[i].college
+		 cout<<student[i].college
 		<<" "<<student[i].major
 		<<" "<<student[i].number
 		<<" "<<student[i].name
